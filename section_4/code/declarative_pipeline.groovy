ffbb3adb3795aa87778e4e59851df2a8c75e3676
vjrmalla <67913193+vjrmalla@users.noreply.github.com>
2020-11-01 23:38:15 +0000

    Update declarative_pipeline.groovy

--- section_4/code/declarative_pipeline.groovy
+++ section_4/code/declarative_pipeline.groovy
@@ -12,9 +12,27 @@ pipeline {
 		}
 
 		stage('Test') {
-			steps {
-				echo 'Testing..'
-			}
+			parallel {
+				stage('Test1')
+				{
+					steps{
+						echo 'Test1'
+					}
+				}
+				stage('Test2'){
+					steps {
+				echo 'Test2..'
+					}
+				}
+				stage('Test3')
+				{
+					steps
+					{
+						echo 'Test3'
+					}
+				}
+			
+		}
 		}
 
 		stage('Deploy') {
